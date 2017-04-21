d84e643f3e75b120d6ed3a9b84df54f749a135da
Fabien Crassat <fabien@crassat.com>
2017-04-21 18:48:53 +0200

    Update Pogdesign-Widgets.user.js

--- scripts/Pogdesign-Widgets.user.js
+++ scripts/Pogdesign-Widgets.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Pogdesign-Widgets
 // @namespace    https://github.com/fabiencrassat
-// @version      1.0.9
+// @version      1.0.11
 // @description  Add links relative to the episode
 // @author       Fabien Crassat <fabien@crassat.com>
 // @match        https://www.pogdesign.co.uk/cat/
