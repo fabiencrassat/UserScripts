73da6f25bac61609fc87a7ae8dc63eef43a005ed
Fabien Crassat <fabien@crassat.com>
2017-12-10 16:22:21 +0100

    Set public the require to greasyfork

--- Pogdesign/Pogdesign-Widgets.user.js
+++ Pogdesign/Pogdesign-Widgets.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Pogdesign-Widgets
 // @namespace    https://github.com/fabiencrassat
-// @version      1.2.1
+// @version      1.3.0
 // @description  Add links relative to the episode
 // @author       Fabien Crassat <fabien@crassat.com>
 // @match        https://www.pogdesign.co.uk/cat/
