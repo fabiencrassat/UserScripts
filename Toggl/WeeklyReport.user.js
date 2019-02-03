704789434062120bd2c59e02c83ae9fe8541397d
Fabien Crassat <fabien@crassat.com>
2019-02-03 20:52:44 +0100

    Redo the toggl job and publish 0.7.0

--- Toggl/WeeklyReport.user.js
+++ Toggl/WeeklyReport.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Toggl - Weekly report
 // @namespace    https://github.com/fabiencrassat
-// @version      0.6.3
+// @version      0.7.0
 // @description  Calculate and display the work day percentages
 // @author       Fabien Crassat <fabien@crassat.com>
 // @include      https://toggl.com/app/*
@@ -12,104 +12,126 @@
 
 /*eslint no-console: ["error", { allow: ["info", "warn", "error"] }] */
 
-/**
- * Remark:
- * The api url is not followed with refresh, need to go and back in the site
- */
+"use strict";
 
-(function() {
-    "use strict";
+const weekDaysPlusTotal = [0, 1, 2, 3, 4, 5, 6, 7];
+const urlToFollow = /^https:\/\/toggl\.com\/app\/reports\/weekly\/\d+\/period\/([a-z])\w+/;
+const apiUrlToFollow = "https://toggl.com/reports/api/v2/weekly.json";
+const decimalLenght = 2;
 
-    const weekDaysPlusTotal = [0, 1, 2, 3, 4, 5, 6, 7];
-    const urlToFollow = /^https:\/\/toggl\.com\/app\/reports\/weekly\/\d+\/period\/([a-z])\w+/;
-    const apiUrlToFollow = "https://toggl.com/reports/api/v2/weekly.json";
-    const displayLinesSelector = "table.data-table tr:not(:first, .subgroup, :last)";
-    const displayColumnsSelector = "td[class^='day-']:not(:has('.fcr-percentage')), td.col-total:not(:has('.fcr-percentage'))";
-    const decimalLenght = 2;
+var oldFetch = fetch; // must be on the global scope
 
-    const percentage = function(numerator, denumerator) {
-        if (!numerator || !denumerator || denumerator === 0) {
-            return 0;
-        }
-        return (numerator / denumerator);
-    };
-
-    const extractOneDay = function(data, dayNumber, totalDay) {
-        var dataDay = [];
-        data.forEach(function(dataLine) {
-            dataDay.push(percentage(dataLine.totals[dayNumber], totalDay));
+const buildFetch = function(doSomethingWithResponse) {
+    fetch = function(url, options) {
+        var promise = oldFetch(url, options);
+        // Do something with the promise
+        promise.then(doSomethingWithResponse).catch(function(error) {
+            console.error("Error in fetch processing", error);
         });
-        return dataDay;
+        return promise;
     };
+}
+const backFetch = function() {
+    fetch = oldFetch;
+}
 
-    function sleep(ms) {
-        return new Promise((resolve) => setTimeout(resolve, ms));
+function sleep() {
+    return new Promise((resolve) => setTimeout(resolve, 1000));
+}
+
+const cleanDisplay = function() {
+    $(".fcr-toggl").remove();
+}
+
+const percentage = function(numerator, denumerator) {
+    if (!numerator || !denumerator || denumerator === 0) {
+        return 0;
     }
+    return (numerator / denumerator);
+};
 
-    async function getDisplayLines() {
-        await sleep(1000); // Need to wait to the table built
-        const displayLines = $(displayLinesSelector);
-        if (!displayLines || displayLines.length === 0) {
-            console.warn("There is no display line", displayLines);
-            return {};
+const extractOneDay = function(data, dayNumber, totalDay) {
+    var dataDay = [];
+    data.forEach(function(dataLine) {
+        dataDay.push(percentage(dataLine.totals[dayNumber], totalDay));
+    });
+    return dataDay;
+};
+
+const calculate = function(weeklyData) {
+    let dataDaysPlusTotal = [];
+    weekDaysPlusTotal.forEach(function(dayOrTotal, index) {
+        dataDaysPlusTotal.push(extractOneDay(weeklyData.data, index, weeklyData.week_totals[index]));
+    });
+
+    let result = [];
+    weeklyData.data.forEach(function(data, index) {
+        let dataDayPlusTotal = dataDaysPlusTotal.map(value => value[index]);
+        result.push({ client: data.title.client, project: data.title.project, data: dataDayPlusTotal });
+    });
+
+    return result;
+}
+
+const filterDataFromProject = function(data, lineElement) {
+    const text = $(lineElement).find(".col-grouping").text();
+    if (text.trim() === '(no project)') {
+        return data.find(value => value.project === null && value.client === null);
+    }
+    return data.find(value => value.project + " " + value.client === text);
+}
+
+const displayInTheLine = function(lineElement, data) {
+    $(lineElement).find("td[class^='day-'], td.col-total").each(function(indexColumn) {
+        const dataInCeil = data.data[indexColumn];
+        if (dataInCeil !== 0) {
+            $(this).append("<p class='fcr-toggl'>" + dataInCeil.toFixed(decimalLenght) + "</p>");
         }
-        return displayLines;
+    });
+}
+
+const display = function(data = []) {
+    const displayLines = $("table.data-table tr:not(:first, .subgroup, :last)");
+    if (!displayLines || displayLines.length === 0) {
+        console.warn("There is no display line", displayLines);
+        return;
     }
+    displayLines.each(function() {
+        displayInTheLine(this, filterDataFromProject(data, this))
+    });
+}
 
-    async function calculateAndDisplay(weeklyData) {
-        // Calculate
-        var dataDaysPlusTotal = [];
-        weekDaysPlusTotal.forEach(function(dayOrTotal, index) {
-            dataDaysPlusTotal.push(extractOneDay(weeklyData.data, index, weeklyData.week_totals[index]));
-        });
+const calculateAndDisplay = async function(data) {
+    await sleep(); // Need to wait to the table built
+    cleanDisplay();
+    display(calculate(data));
+}
 
-        // Display
-        getDisplayLines().then(function(displayLines) {
-            if (displayLines.length !== dataDaysPlusTotal[0].length) {
-                console.warn("There is not the same calculated and display lines.", displayLines, dataDaysPlusTotal[0]);
-                return;
-            }
-            displayLines.each(function(indexLine) {
-                const element = $(this).find(displayColumnsSelector);
-                element.each(function(indexColumn) {
-                    const dataInCeil = dataDaysPlusTotal[indexColumn][displayLines.length - indexLine - 1];
-                    if (dataInCeil !== 0) {
-                        $(this).append("<p class='fcr-percentage'>" + dataInCeil.toFixed(decimalLenght) + "</p>");
-                    }
-                });
-            });
-        });
+const response = function(response) {
+    const responseClone = response.clone(); // clone to consume json body stream response
+    const url = response.url;
+    if (responseClone.ok && responseClone.status === 200 && url && url.startsWith(apiUrlToFollow)) {
+        console.info("Url to follow found!", url);
+        responseClone.json().then(calculateAndDisplay);
     }
+}
 
-    const main = function(currentUrl) {
-        if (urlToFollow.test(currentUrl)) {
-            var oldFetch = fetch; // must be on the global scope
-            fetch = function(url, options) {
-                var promise = oldFetch(url, options);
-                // Do something with the promise
-                promise.then(function(response) {
-                    const responseClone = response.clone(); // clone to consume json body stream response
-                    const url = response.url;
-                    if (responseClone.ok && responseClone.status === 200 && url && url.startsWith(apiUrlToFollow)) {
-                        console.info("Url to follow found!", url);
-                        responseClone.json().then(function(data) {
-                            calculateAndDisplay(data);
-                        });
-                    }
-                }).catch(function(error) {
-                    console.error("Error in fetch processing", error);
-                });
-                return promise;
-            };
-        }
+const fireOnChange = function(url = '') {
+    // Check if we are in the good page
+    if (urlToFollow.test(url)) {
+        buildFetch(response);
+        return true;
+    }
+    backFetch();
+    return false;
+}
+
+console.info("== Toggl - Weekly report ==");
+// Follow the HTML5 url change in the API browser
+(function (old) {
+    window.history.pushState = function () {
+        old.apply(window.history, arguments);
+        fireOnChange(window.location.href);
     };
-
-    console.info("== Toggl - Weekly report ==");
-    // Follow the HTML5 url change in the API browser
-    (function (old) {
-        window.history.pushState = function () {
-            old.apply(window.history, arguments);
-            main(window.location.href);
-        };
-    }(window.history.pushState));
-}());
+}(window.history.pushState));
+fireOnChange(location.href);
