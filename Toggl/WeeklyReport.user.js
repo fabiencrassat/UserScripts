23f431a5a61b6add572f9f4fb63b99a20474f68f
Fabien Crassat <fabien@crassat.com>
2019-02-03 22:49:43 +0100

    Fixes from Codacy #2

--- Toggl/WeeklyReport.user.js
+++ Toggl/WeeklyReport.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Toggl - Weekly report
 // @namespace    https://github.com/fabiencrassat
-// @version      0.7.1
+// @version      0.7.2
 // @description  Calculate and display the work day percentages
 // @author       Fabien Crassat <fabien@crassat.com>
 // @include      https://toggl.com/app/*
@@ -30,18 +30,18 @@ const buildFetch = function(doSomethingWithResponse) {
         });
         return promise;
     };
-}
+};
 const backFetch = function() {
     fetch = oldFetch;
-}
+};
 
 function sleep() {
     return new Promise((resolve) => setTimeout(resolve, 1000));
-}
+};
 
 const cleanDisplay = function() {
     $(".fcr-toggl").remove();
-}
+};
 
 const percentage = function(numerator, denumerator) {
     if (!numerator || !denumerator || denumerator === 0) {
@@ -71,7 +71,7 @@ const calculate = function(weeklyData) {
     });
 
     return result;
-}
+};
 
 const filterDataFromProject = function(data, lineElement) {
     const text = $(lineElement).find(".col-grouping").text();
@@ -79,7 +79,7 @@ const filterDataFromProject = function(data, lineElement) {
         return data.find((value) => value.project === null && value.client === null);
     }
     return data.find((value) => value.project + " " + value.client === text);
-}
+};
 
 const displayInTheLine = function(lineElement, data) {
     $(lineElement).find("td[class^='day-'], td.col-total").each(function(indexColumn) {
@@ -88,7 +88,7 @@ const displayInTheLine = function(lineElement, data) {
             $(this).append("<p class='fcr-toggl'>" + dataInCeil.toFixed(decimalLenght) + "</p>");
         }
     });
-}
+};
 
 const display = function(data = []) {
     const displayLines = $("table.data-table tr:not(:first, .subgroup, :last)");
@@ -99,24 +99,26 @@ const display = function(data = []) {
     displayLines.each(function() {
         displayInTheLine(this, filterDataFromProject(data, this));
     });
-}
+};
 
 const calculateAndDisplay = async function(data) {
     await sleep(); // Need to wait to the table built
     cleanDisplay();
     display(calculate(data));
-}
+};
+
+const checkResponseAndUrl = function(response, url) {
+    return response.ok && response.status === 200 && url && url.startsWith(apiUrlToFollow);
+};
 
 const response = function(response) {
-    const responseClone = response.clone(); // clone to consume json body stream response
-    if(!responseClone.ok || responseClone.status !== 200) return;
-    
+    const responseClone = response.clone(); // clone to consume json body stream response    
     const url = response.url;
-    if (url && url.startsWith(apiUrlToFollow)) {
+    if (checkResponseAndUrl(responseClone, url)) {
         console.info("Url to follow found!", url);
         responseClone.json().then(calculateAndDisplay);
     }
-}
+};
 
 const fireOnChange = function(url = "") {
     // Check if we are in the good page
