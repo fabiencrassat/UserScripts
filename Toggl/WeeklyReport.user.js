0e739ec3d44b836349792d184104948aa3cbb291
Fabien Crassat <fabien@crassat.com>
2019-05-15 17:39:03 +0200

    Upgrade to V3 api

--- Toggl/WeeklyReport.user.js
+++ Toggl/WeeklyReport.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Toggl - Weekly report
 // @namespace    https://github.com/fabiencrassat
-// @version      0.7.3
+// @version      0.8.0
 // @description  Calculate and display the work day percentages
 // @author       Fabien Crassat <fabien@crassat.com>
 // @include      https://toggl.com/app/*
@@ -14,12 +14,22 @@
 
 "use strict";
 
-const weekDaysPlusTotal = [0, 1, 2, 3, 4, 5, 6, 7];
+const weekDays = [0, 1, 2, 3, 4, 5, 6];
 const urlToFollow = /^https:\/\/toggl\.com\/app\/reports\/weekly\/\d+\/period\/([a-z])\w+/;
-const apiUrlToFollow = "https://toggl.com/reports/api/v2/weekly.json";
+const apiTimeEntriesUrlToFollow = "https://toggl.com/reports/api/v3/workspace/2294752/weekly/time_entries";
+const apiProjectsUrlToFollow = "https://toggl.com/api/v9/me/projects";
 const decimalLenght = 2;
+const debugMode = false;
+
+let projects = [];
 
 var oldFetch = fetch; // must be on the global scope
+
+const consoleDebug = function() {
+  if (debugMode) {
+    console.log(arguments);
+  }
+}
 
 const buildFetch = function(doSomethingWithResponse) {
     fetch = function(url, options) {
@@ -50,48 +60,96 @@ const percentage = function(numerator, denumerator) {
     return (numerator / denumerator);
 };
 
-const extractOneDay = function(data, dayNumber, totalDay) {
-    var dataDay = [];
-    data.forEach(function(dataLine) {
-        dataDay.push(percentage(dataLine.totals[dayNumber], totalDay));
-    });
-    return dataDay;
-};
-
 const calculate = function(weeklyData) {
-    let dataDaysPlusTotal = [];
-    weekDaysPlusTotal.forEach(function(dayOrTotal, index) {
-        dataDaysPlusTotal.push(extractOneDay(weeklyData.data, index, weeklyData.week_totals[index]));
+    consoleDebug('calculate', weeklyData);
+    /**
+     * The weeklyData argument in the V3 API has this values
+     * [
+     *   {
+     *     "user_id": 2644339,
+     *     "project_id": 150741509,
+     *     "seconds": // Mon, Tue, Wed, Thu, Fri, Sat, Sun
+     *       [0, 7200, 0, 0, 0, 0, 0],
+     *   },
+     *   {...}
+     * ]
+     *
+     * The result:
+     * [
+     *   {
+     *     client: '',
+     *     project: 'name',
+     *     data: // Mon, Tue, Wed, Thu, Fri, Sat, Sun
+     *       [0, 0.23, 0, 0, 0, 0, 0],
+     *     conso: sum(weeklyData[].seconds) / sum(allProjectsDays),
+     *   },
+     *   {...}
+     * ]
+     */
+
+    let projectSum = {};
+    let daysSum = [];
+    weeklyData.forEach(function(line) {
+      // Sum the line
+      projectSum[line.project_id] = line.seconds.reduce(function(acc, cur) {
+        return acc + cur;
+      });
+      // Sum the days
+      weekDays.forEach(function(day) {
+        daysSum[day] = (daysSum[day] || 0) + line.seconds[day];
+      });
+    });
+
+    // Sum the week
+    let weekSum = daysSum.reduce(function(acc, cur) {
+      return acc + cur;
     });
 
     let result = [];
-    weeklyData.data.forEach(function(data, index) {
-        let dataDayPlusTotal = dataDaysPlusTotal.map((value) => value[index]);
-        result.push({ client: data.title.client, project: data.title.project, data: dataDayPlusTotal });
+    weeklyData.forEach(function(line) {
+      let data = [];
+      line.seconds.forEach(function(day, index) {
+        data.push(percentage(day, daysSum[index]))
+      });
+      result.push({
+        client: '',
+        project: projects.find(project => project.id === line.project_id).name,
+        data: data,
+        conso: percentage(projectSum[line.project_id], weekSum)
+      });
     });
 
+    consoleDebug('calculate', result);
     return result;
 };
 
 const filterDataFromProject = function(data, lineElement) {
-    const text = $(lineElement).find(".col-grouping").text();
+    const text = $(lineElement).find(".css-70qvj9.efdmxuc2 > span:first-child").text();
     if (text.trim() === "(no project)") {
         return data.find((value) => value.project === null && value.client === null);
     }
-    return data.find((value) => value.project + " " + value.client === text);
+    return data.find((value) => value.project === text);
 };
 
 const displayInTheLine = function(lineElement, data) {
-    $(lineElement).find("td[class^='day-'], td.col-total").each(function(indexColumn) {
-        const dataInCeil = data.data[indexColumn];
+    // For each line, select only days and total columns
+    const columns = $(lineElement).find(".css-1xn8q1e.euf6jrl1");
+    columns.each(function(indexColumn) {
+        let dataInCeil = 0;
+        if (columns.length === indexColumn + 1) {
+          dataInCeil = data.conso;
+        } else {
+          dataInCeil = data.data[indexColumn];
+        }
         if (dataInCeil !== 0) {
-            $(this).append("<p class='fcr-toggl'>" + dataInCeil.toFixed(decimalLenght) + "</p>");
+            $(this).append("<p class='fcr-toggl'><br />" + dataInCeil.toFixed(decimalLenght) + "</p>");
         }
     });
 };
 
 const display = function(data = []) {
-    const displayLines = $("table.data-table tr:not(:first, .subgroup, :last)");
+    // Select the data line in the tab
+    const displayLines = $(".css-1v0lzu.euf6jrl0:not(:first, :last)");
     if (!displayLines || displayLines.length === 0) {
         console.warn("There is no display line", displayLines);
         return;
@@ -107,14 +165,24 @@ const calculateAndDisplay = async function(data) {
     display(calculate(data));
 };
 
-const checkResponseAndUrl = function(response, url) {
-    return response.ok && response.status === 200 && url && url.startsWith(apiUrlToFollow);
+const fillProjects = async function(data = []) {
+    projects = data.map(project => {
+        return { id: project.id, name: project.name, clientId : project.client_id }
+    })
+}
+
+const checkResponseAndUrl = function(response, url, apiRUl) {
+    return response.ok && response.status === 200 && url && url.startsWith(apiRUl);
 };
 
 const response = function(response) {
-    const responseClone = response.clone(); // clone to consume json body stream response    
+    const responseClone = response.clone(); // clone to consume json body stream response
     const url = response.url;
-    if (checkResponseAndUrl(responseClone, url)) {
+    if (checkResponseAndUrl(responseClone, url, apiProjectsUrlToFollow)) {
+        console.info("Url to follow found!", url);
+        responseClone.json().then(fillProjects);
+    }
+    if (checkResponseAndUrl(responseClone, url, apiTimeEntriesUrlToFollow)) {
         console.info("Url to follow found!", url);
         responseClone.json().then(calculateAndDisplay);
     }
@@ -123,9 +191,11 @@ const response = function(response) {
 const fireOnChange = function(url = "") {
     // Check if we are in the good page
     if (urlToFollow.test(url)) {
+        consoleDebug('It is the good url', url);
         buildFetch(response);
         return true;
     }
+    consoleDebug('It is not the good url', url);
     backFetch();
     return false;
 };
