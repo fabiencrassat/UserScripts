6bb542dee9017606b23078f6425dfe8911192b35
Fabien Crassat <fabien@crassat.com>
2020-04-10 19:48:24 +0200

    Decrease line numbers
    Update Subscene version

--- Subscene/Subscene.user.js
+++ Subscene/Subscene.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Subscene
 // @namespace    https://github.com/fabiencrassat
-// @version      0.1.2
+// @version      0.1.3
 // @description  Filter with english language!
 // @author       Fabien Crassat <fabien@crassat.com>
 // @match        https://subscene.com/*
@@ -12,38 +12,38 @@
 /* global Cookies */
 'use strict';
 
-(function subscene() {
-  const SEPARATOR_VALUES = ',';
+const SEPARATOR_VALUES = ',';
 
-  const languages = {
-    arePresents() {
-      const languageFilterCookie = Cookies.get(languages.key);
-      if (!languageFilterCookie) {
-        return false;
-      }
-      if (languageFilterCookie !== languages.getKeyValues()) {
-        return false;
-      }
-      return true;
-    },
-    domain: '.subscene.com',
-    getKeyValues() {
-      return Object.keys(languages.values).join(SEPARATOR_VALUES);
-    },
-    key: 'LanguageFilter',
-    path: '/',
-    setCookie() {
-      Cookies.set(languages.key, languages.getKeyValues(), {
-        domain: languages.domain,
-        path: languages.path,
-        secure: true
-      });
-    },
-    values: {
-      13: 'english'
+const languages = {
+  arePresents() {
+    const languageFilterCookie = Cookies.get(languages.key);
+    if (!languageFilterCookie) {
+      return false;
     }
-  };
+    if (languageFilterCookie !== languages.getKeyValues()) {
+      return false;
+    }
+    return true;
+  },
+  domain: '.subscene.com',
+  getKeyValues() {
+    return Object.keys(languages.values).join(SEPARATOR_VALUES);
+  },
+  key: 'LanguageFilter',
+  path: '/',
+  setCookie() {
+    Cookies.set(languages.key, languages.getKeyValues(), {
+      domain: languages.domain,
+      path: languages.path,
+      secure: true
+    });
+  },
+  values: {
+    13: 'english'
+  }
+};
 
+(function subscene() {
   /* Main */
   if (languages.arePresents()) {
     return;
